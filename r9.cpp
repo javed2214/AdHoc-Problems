e34cac8bf2088bb217d414cc6edeb1c968a28665
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-01-04 01:37:41 +0530

    Add files via upload

---
+++ r9.cpp
@@ -0,0 +1,39 @@
+// https://www.codechef.com/UCS2018/problems/UCS01
+
+#include<bits/stdc++.h>
+using namespace std;
+#define SIZE 10000
+
+void findAns(std::vector<int> &V){
+
+	for(int i=0;i<V.size()-1;i++){
+		if(abs(V[i]-V[i+1])>1){
+			swap(V[i],V[i+1]);
+			for(int j=i;j>0;j--){
+				if(abs(V[j]-V[j-1])>1)
+					swap(V[j],V[j-1]);
+			}
+		}
+	}
+}
+int main(){
+
+	int test;
+	cin>>test;
+	while(test--){
+		int n,x;
+		cin>>n;
+		std::vector<int> V;
+		for(int i=0;i<n;i++){
+			cin>>x;
+			V.push_back(x);
+		}
+		findAns(V);
+
+		for(int i=0;i<V.size();i++)
+			cout<<V[i]<<" ";
+		cout<<endl;
+	}
+
+	return 0;
+}
